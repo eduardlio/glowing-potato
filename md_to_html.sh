14448fd985274ec62cc954c10373d9fc2af3ae32
eduardlio <liao.edward@gmail.com>
2017-12-28 11:20:10 +1000

    Added functionality if stuff didn't already exist, still need to comment out git commands

--- md_to_html.sh
+++ md_to_html.sh
@@ -1,15 +1,33 @@
+message=""
+
 if [[ ! -d html ]]; then
+   echo "directory html doesn't exist yet, creating directory html"
    mkdir html
-   git add .
 fi;
+if [[ ! -f html/output.html ]]; then
+   message="Added html/output.html"
+else
+   message="Updated html/output.html"
+fi;
+
 htmlfile=html/output.html
+
 echo '<link rel="stylesheet" type="text/css" href="../css/main.css">' > $htmlfile
 echo '<div class="content">' >> $htmlfile;
+
+if [[ ! -f resume.md ]]; then
+   echo "resume.md file not found, creating one now"
+   sh ./editResume.sh;
+fi;
+
+echo "Converting resume.md to html ( location: $htmlfile )"
 pandoc -f markdown -t html -o $htmlfile.tmp resume.md
+
 echo '</div>'>> $htmlfile.tmp;
 cat $htmlfile.tmp >> $htmlfile;
 rm $htmlfile.tmp;
+echo $message;
 
-echo "HTML file update in html/output.html";
-git commit -am "Updated html resume";
-git push origin master;
+# git add html/output.html
+# git commit -am "$message";
+# git push origin master;
